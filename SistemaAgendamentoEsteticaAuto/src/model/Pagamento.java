b890544f66d6b2ff19b3497f3f4f49962f963717
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-12-07 19:56:38 -0300

    [FEAT] Implementando construtores, métodos, getters/setters a classe Pagamento

--- SistemaAgendamentoEsteticaAuto/src/model/Pagamento.java
+++ SistemaAgendamentoEsteticaAuto/src/model/Pagamento.java
@@ -1,15 +1,43 @@
 package model;
 
 public class Pagamento {
-    private int formaDePagamento; // 1- pix, 2 - credito, 3 - debito, 4 - dinheiro
-    private double valorTotal;
+    private FormaPagamento formaPagamento; 
+    private StatusPagamento status;
+    private double valor;
     
-    public Pagamento(int tipoDePagamento, double valorTotal){
-        this.formaDePagamento = tipoDePagamento;
-        this.valorTotal = valorTotal;
+    public Pagamento(double valor, FormaPagamento formaPagamento, StatusPagamento status){
+        this.formaPagamento = formaPagamento;
+        this.status = status;
+        this.valor = valor;
     }
 
-    public boolean pago(){
-        return false;
+    public Pagamento(double valor, FormaPagamento formaPagamento){
+        this.valor = valor;
+        this.formaPagamento = formaPagamento;
+        this.status = status.PENDENTE;
+    }
+
+    public FormaPagamento getFormaPagamento(){return this.formaPagamento;}
+    public void setFormaPagamento(FormaPagamento formaPagamento){this.formaPagamento = formaPagamento;}
+
+    public StatusPagamento getStatusPagamento(){return this.status;}
+    public void setStatusPagamento(StatusPagamento status){this.status = status;}
+
+    public double getValor(){return this.valor;}
+    public void setValor(double valor){this.valor = valor;}
+
+    @Override
+    public String toString(){
+        return "--- Pagamento ---\n" + "Valor: R$" + String.format("%.2f", valor) + "\nForma de Pagamento: " + formaPagamento + "\nStatus: " + status + "\n";
+    }
+
+    // Testes
+    public static void main(String[] args) {
+        Pagamento pag = new Pagamento(500, FormaPagamento.DINHEIRO, StatusPagamento.PENDENTE);
+        System.out.println(pag.toString());
+        pag.setStatusPagamento(StatusPagamento.PAGO);
+        System.out.println(pag.toString());
+        pag.setFormaPagamento(FormaPagamento.CREDITO);
+        System.out.println(pag.toString());
     }
 }
