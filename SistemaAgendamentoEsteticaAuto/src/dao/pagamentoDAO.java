a27f442887ce28a180ff398f2db892178b2b19fe
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-12-11 10:09:33 -0300

    [FEAT] Implementando método salvar na classe pagamentoDAO

--- SistemaAgendamentoEsteticaAuto/src/dao/pagamentoDAO.java
+++ SistemaAgendamentoEsteticaAuto/src/dao/pagamentoDAO.java
@@ -2,14 +2,19 @@ package dao;
 
 import java.util.List;
 
+import model.FormaPagamento;
 import model.Pagamento;
+import model.StatusPagamento;
 
-public class pagamentoDAO implements IDAO<Pagamento>{
+public class PagamentoDAO implements IDAO<Pagamento>{
 
     private static final String ARQUIVO = "pagamentos.txt";
 
     public boolean salvar(Pagamento pagamento){
-
+        String linha = pagamento.getValor() + ";"+
+        pagamento.getFormaPagamento() + ";" +
+        pagamento.getStatusPagamento();
+        GerenciadorDeArquivos.salvar(ARQUIVO, linha);
         return true;
     }
 
@@ -17,7 +22,8 @@ public class pagamentoDAO implements IDAO<Pagamento>{
         throw new UnsupportedOperationException("Unimplemented method 'listar'");
     }
 
-    public boolean atualizar(Pagamento pagamento){
+    public boolean atualizar(Pagamento objeto){
+        
         return true;
     }
 
@@ -30,6 +36,13 @@ public class pagamentoDAO implements IDAO<Pagamento>{
         return null;
     }
 
+public static void main(String[] args) {
+    Pagamento pagamento = new Pagamento(1000, FormaPagamento.CREDITO, StatusPagamento.PAGO);
+    Pagamento pagamento2 = new Pagamento(2345, FormaPagamento.DINHEIRO, StatusPagamento.PENDENTE);
+    PagamentoDAO dao = new PagamentoDAO();
 
-    
+    dao.salvar(pagamento);
+    dao.salvar(pagamento2);
+}
+
 }
