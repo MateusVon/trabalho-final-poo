5ee686c0be770c6a6fb28c95ca0aa2b528924c91
Eduardo Coelho <eduardohrcoelhomkt@gmail.com>
2025-12-11 14:34:36 -0300

    [FEAT] Implementando métodos listar, atualizar, deletar e buscarPorId na classa pagamentoDAO

--- SistemaAgendamentoEsteticaAuto/src/model/Pagamento.java
+++ SistemaAgendamentoEsteticaAuto/src/model/Pagamento.java
@@ -4,6 +4,7 @@ public class Pagamento {
     private FormaPagamento formaPagamento; 
     private StatusPagamento status;
     private double valor;
+    private int id;
     
     public Pagamento(double valor, FormaPagamento formaPagamento, StatusPagamento status){
         this.formaPagamento = formaPagamento;
@@ -20,11 +21,14 @@ public class Pagamento {
     public FormaPagamento getFormaPagamento(){return this.formaPagamento;}
     public void setFormaPagamento(FormaPagamento formaPagamento){this.formaPagamento = formaPagamento;}
 
-    public StatusPagamento getStatusPagamento(){return this.status;}
-    public void setStatusPagamento(StatusPagamento status){this.status = status;}
+    public StatusPagamento getStatus() {return status;}
+    public void setStatus(StatusPagamento status) {this.status = status;}
 
     public double getValor(){return this.valor;}
     public void setValor(double valor){this.valor = valor;}
+
+    public int getId() {return id;}
+    public void setId(int id) {this.id = id;}
 
     public void confirmarPagamento(){
         this.status = StatusPagamento.PAGO;
@@ -32,14 +36,14 @@ public class Pagamento {
 
     @Override
     public String toString(){
-        return "--- Pagamento ---\n" + "Valor: R$" + String.format("%.2f", valor) + "\nForma de Pagamento: " + formaPagamento + "\nStatus: " + status + "\n";
+        return "--- Pagamento ---\n" + "ID do Pagamento: " + id + "\nValor: R$" + String.format("%.2f", valor) + "\nForma de Pagamento: " + formaPagamento + "\nStatus: " + status + "\n";
     }
 
     // Testes
     public static void main(String[] args) {
         Pagamento pag = new Pagamento(500, FormaPagamento.DINHEIRO, StatusPagamento.PENDENTE);
         System.out.println(pag.toString());
-        pag.setStatusPagamento(StatusPagamento.PAGO);
+        pag.setStatus(StatusPagamento.PAGO);
         System.out.println(pag.toString());
         pag.setFormaPagamento(FormaPagamento.CREDITO);
         System.out.println(pag.toString());
